7b4be0eec959c4195a485aaf3c0e01a695e416af
neuron <praveen.902012@gmail.com>
2024-05-25 23:05:21 +0530

    added GTag WIP

--- src/App.js
+++ src/App.js
@@ -18,15 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0DLEPE47ZD"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
-
-            gtag('config', 'G-0DLEPE47ZD');
-        </script>
         </div>
 
     );

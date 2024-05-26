565ffc07c6bf762b65402f8baf53b2fa8cefc838
neuron <praveen.902012@gmail.com>
2024-05-26 09:11:19 +0530

    added GTag WIP

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
-const TRACKING_ID = "G-0DLEPE47ZD"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
-function App() {
-    return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        </div>
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+const TRACKING_ID = "UA-220438183-5"; // OUR_TRACKING_ID
 
-    );
+ReactGA.initialize(TRACKING_ID);
+
+const ContactUs = () => (
+    <div>
+      <h3>Contact Us</h3>
+    </div>
+);
+
+const Home = () => (
+    <div>
+      <h2>Home</h2>
+    </div>
+);
+
+const About = () => (
+    <div>
+      <h2>About</h2>
+    </div>
+);
+
+const App = () => {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contactus" element={<ContactUs />} />
+        </Routes>
+      </Router>
+  );
 }
 
 export default App;
